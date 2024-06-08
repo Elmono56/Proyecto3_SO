84039b0140e33970fdf3d66274cb51a659dc100f
Elmono56 <hidalgo.pablo56@gmail.com>
2024-06-07 23:11:38 -0600

    first approach to structure of code

---
+++ bftp.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <pthread.h>
+
+#define PORT 8889
+#define BUFFER_SIZE 1024
+
+void server(){
+
+};
+
+
+
+void client(){
+
+};
+
+
+
+void handle_conexion(){
+
+};
+
+
+
+int main(){
+
+
+
+    return 0;
+};
+
+
+/*
+    open <dirección-ip>: establece una conexión remota
+	close: cierra la conexión actual
+	quit: termina el programa
+	cd <directorio>: cambia de directorio remoto
+	get <archivo>: recupera un archivo remoto
+	lcd <directorio>: cambia de directorio local
+	ls: lista los archivos del directorio remoto
+	put <archivo>: envía un archivo a la máquina remota
+	pwd: muestra el directorio activo remoto
+*/
